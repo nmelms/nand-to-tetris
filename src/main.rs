3f44363d96a1592d7bc65115dc778e2d4a796e71
Nick Melms <nmelms92@gmail.com>
2026-01-02 12:02:04 -0800

    create parser file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use std::env;
 use std::fs;
+mod parser;
 // parser
 //code
 //assmbler
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    let file_name = &args[1];
+    // let file_name = &args[1];
 
-    let contents = fs::read_to_string(file_name)
-        .expect("there was an error reading from the file");
+    parser::hello();
+
+    // let contents = fs::read_to_string(file_name)
+    //     .expect("there was an error reading from the file");
 
 
-    println!("{:?}", contents);
+    // println!("{:?}", contents);
 }
