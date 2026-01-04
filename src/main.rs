2b0f637024cb3d9478dce6c87d2ecd5ac08e2a61
Nick Melms <nmelms92@gmail.com>
2026-01-04 15:22:07 -0800

    create dest

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
 use std::env;
 use std::fs;
 mod parser;
+
 // parser
 //code
 //assmbler
 fn main() {
-
     let args: Vec<String> = env::args().collect();
     let file_name = &args[1];
 
+    let mut parser = parser::Parser::new(file_name);
+    println!("{:?}", parser.instruction_type());
 
-    let contents = parser::Parser::init(file_name);
-    println!("{:?}", contents.instruction_type());
-
-    // let contents = fs::read_to_string(file_name)
-    //     .expect("there was an error reading from the file");
-
-
-    // println!("{:?}", contents);
+    while parser.has_more_lines() {
+        let curr = parser.current_index;
+        // println!("THis is the current lline: {}", parser.lines[curr]);
+        let dest = parser.dest();
+        println!("
+        Curr: {}
+        Dest: {}
+        ", curr,dest);
+        parser.advance();
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -3,47 +3,72 @@ use std::fs;
 #[derive(Debug)]
 pub struct Parser {
     file_contents: String,
-    lines: Vec<String>,
-    current_index: usize,
+    pub lines: Vec<String>,
+    pub current_index: usize,
 }
 
 impl Parser {
-    pub fn init(file_name: &str) -> Self {
-     let file_contents: String = fs::read_to_string(file_name).expect("there was an error reading from the file in parser");
-     let mut current_index = 0;
-     let mut lines = Vec::new();
+    pub fn new(file_name: &str) -> Self {
+        let file_contents: String = fs::read_to_string(file_name)
+            .expect("there was an error reading from the file in parser");
+        let mut current_index = 0;
+        let mut lines = Vec::new();
 
-     for line in file_contents.lines(){
-        lines.push(line.to_string());
-     }
+        for line in file_contents.lines() {
+            lines.push(line.to_string());
+        }
 
-     Self {file_contents, current_index, lines}
-    
+        Self {
+            file_contents,
+            current_index,
+            lines,
+        }
     }
 
-    pub fn has_more_lines(&self) -> bool{
-        if self.current_index < self.file_contents.len()  {
-            true 
-        }  else{
+    pub fn has_more_lines(&self) -> bool {
+        if self.current_index < self.file_contents.len() {
+            true
+        } else {
             false
-        } 
-
+        }
     }
 
-    pub fn advance(&mut self){
+    pub fn advance(&mut self) {
         self.current_index += 1
     }
 
-    pub fn instruction_type(&self) -> String{
-        if self.lines[self.current_index].starts_with("@"){
+    pub fn instruction_type(&self) -> String {
+        if self.lines[self.current_index].starts_with("@") {
             String::from("A_INSTRUCTION")
-        }else if self.lines[self.current_index].starts_with("symbol"){
+        } else if self.lines[self.current_index].starts_with("symbol") {
             String::from("L_INSTRUCTION")
-        }else{
+        } else {
             String::from("C_INSTRUCTION")
         }
-    }   
+    }
+
+    pub fn symbol(&self) -> Option<String> {
+        if self.instruction_type() == "A_INSTRUCTION" || self.instruction_type() == "L_INSTRUCTION"
+        {
+            Some(self.lines[self.current_index].clone())
+        } else {
+            None
+        }
+    }
+
+    pub fn dest(&self) -> &str {
+        //get current line
+        if &self.instruction_type() == "C_INSTRUCTION" {
+            let cur = &self.lines[self.current_index];
+            let has_dest: bool = self.lines[self.current_index].contains("=");
+            let split: Vec<&str> = cur.split("=").collect();
+            if has_dest {
+                split[0]
+            } else {
+                "This instruction does not have a dest"
+            }
+        }else{
+            "This is not an A instruction"
+        }
+    }
 }
-
-
-
